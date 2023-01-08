a41037e9f5ddd3d01fed8efd921d149a8d7c9b4e
Hardbass1991 <midailsorridente@gmail.com>
2023-01-08 18:24:31 -0500

    change setup file

--- module3_task1/setup.sh
+++ module3_task1/setup.sh
@@ -1,6 +1,2 @@
 #!/bin/bash
-<<<<<<< HEAD
 apt-get update && apt-get install -y hugo make shellcheck
-=======
-apt-get update && apt-get install -y hugo make
->>>>>>> 87fdbd16aeb1a7172fd74fef6e6cca65b09d97a8
